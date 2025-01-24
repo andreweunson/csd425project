f2f43338d1a8df15f62a1ca114c88b5abde32ae2
andreweunson <131382715+andreweunson@users.noreply.github.com>
2025-01-23 21:57:08 -0800

    Added preview of class 'descendants

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
+/**
+ * Goals:
+ *  1. Classes are automatically grey before we click on a start button
+    2. Clicking the start button signifies that we have 'started' the program, and any available classes (ie. those without prerequisites) should all turn white
+    3. Clicking on a white class should make all prerequisite classes (those needed to be taken before the clicked class can be taken) white
+ */
+
+// Declarations
+let taken = [];
+let available = ["100", "101", "102", "103", "106", "109", "112"];
+let greyClassList = ["104", "105", "107", "108", "110", "111", "113", "114", "115", "116", "117", "118", "119"];
+let classReqAncestors = {
+    "104": ["100"],
+    "105": ["100"]
+};
+let classReqDescendants = {
+    "100": ["104", "105"]
+}
+
+// Start button functionality
 const start_btn = document.getElementById('start_btn');
-
 let start = false;
-
-//dictionary of taken classes
-let taken = {};
-
-// no prereq classes
-let noPrereqClasses = ["105", "107"];
-
-// A list of classes for which key class is a prerequisite
-let prereq = {
-    "105": ["103", "110"],
-    "107": ["109", "110"]
-}
-
-// A list of classes which are prerequisites for a key class
-let classPrerequisites = {
-    "103": ["105"],
-    "109": ["107"],
-    "110": ["105", "107"],
-}
-
-document.querySelectorAll(".lesson").forEach(lesson => {
-    lesson.addEventListener("click", () => {
-        // When clicked, if start button was clicked before (start=true) and current class is available
-        if (start && lesson.classList.contains("available")) {
-            taken[lesson.id] = true;
-
-            // change bg color to green
-            lesson.classList.add("taken");
-            lesson.classList.remove("available");
-
-            // if class is prerequisite
-            if (prereq[lesson.id]) {
-                // for each class where this class is prereq
-                prereq[lesson.id].forEach((dependableClass) => {
-
-                    // assume dependableClass can be made available
-                    let allPrereqTaken = true;
-
-                    // Check all prerequisites for dependableClass to see if any of them is not taken yet
-                    classPrerequisites[dependableClass].forEach(classe => {
-                        if (!taken[classe]) {
-                            allPrereqTaken = false;
-                        }
-                    });
-
-                    if (allPrereqTaken) {
-                        document.getElementById(dependableClass).classList.add("available");
-                    }
-                })
-            }
-        }
-    })
-});
-
 start_btn.addEventListener("click", function () {
     start = true;
-    noPrereqClasses.forEach(value => {
+    available.forEach(value => {
         document.getElementById(value).classList.add("available");
     })
 });
+
+
+
+document.querySelectorAll(".lesson").forEach(lesson => {
+    lesson.addEventListener("click", () => {
+        //Only want to give descendant preview when we've 1. started the program and 2. the class we've clicked is a white class
+        if(start && lesson.classList.contains("available")) {
+
+            //Reset all to be grey according to available array
+            available.forEach(value => {
+                document.getElementById(value).classList.remove("preview");
+                document.getElementById(value).classList.add("available");
+            })
+
+            //Ensure any previous descendants get turned back to grey
+            greyClassList.forEach(value => {
+              document.getElementById(value).classList.remove("available");
+              document.getElementById(value).classList.remove("preview");
+            });
+			
+            //Turn clicked class orange for debugging purposes
+            lesson.classList.remove("available");
+            lesson.classList.add("preview");
+            
+
+            //If it has classes that require it, make them all white
+            if(classReqDescendants[lesson.id]) {
+                classReqDescendants[lesson.id].forEach(reqClass => {
+                    document.getElementById(reqClass).classList.add("available");
+                })
+            }
+        }
+    });
+});
+    
+
+
